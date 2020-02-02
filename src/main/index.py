a93f2d0cd2ef8fbdca76d089298a8acd2009a71e
SAWET.LA <sawet.la@GLSHMSDLT18.local>
2020-02-02 22:14:35 +0700

    Change callback interfacing between Python and Spring Cloud Stream.

--- src/main/index.py
+++ src/main/index.py
@@ -1,3 +1,14 @@
+"""
+@author : "Savage Lasa"
+@copyright : "Copyright 2020, The Python Spring Cloud Stream Project"
+@license : "GPL"
+@version : "1.0.1"
+@maintainer : "Savage Lasa"
+@email : "superoutput@gmail.com"
+@status : "Production"
+"""
+
+import sys
 from py4j.java_gateway import JavaGateway, java_import, get_field, CallbackServerParameters
 from py4j.clientserver import ClientServer, JavaParameters, PythonParameters
 
@@ -9,8 +20,8 @@ java_import(gateway.jvm, 'io.hms.mda.stream.spring.python.*')
 class PythonCallbackImpl(object):
     def __init__(self, execfunc):
         self.execfunc = execfunc
-    def notify(self, obj):
-        print('[PythonCallbackImpl] notified from Java')
+    def notify(self, obj, event):
+        print('[PythonCallbackImpl] notified from Java', event)
         self.execfunc()
         return 'dummy return value'
     class Java:
@@ -20,16 +31,37 @@ def simple_fun():
     print('[simple_fun] called')
     gateway.jvm.System.out.println("[simple_fun] Hello from python!")
 
-# Test 1: Without threading
-input('Ready to begin test 1')
-python_callback = PythonCallbackImpl(simple_fun)
-nothread_executor = gateway.jvm.Test(python_callback)
-nothread_executor.runSynchronous()
 
-# Test 2: With threading
-input('Ready to begin test 2')
-python_callback = PythonCallbackImpl(simple_fun)
-nothread_executor = gateway.jvm.Test(python_callback)
-nothread_executor.runAsynchronous()
+def main():
+    print('Start streaming...')
+    while True:
 
-#gateway.shutdown()
+        try:
+            command = input("> ")
+            print('Command: ', command)
+            if command == "exit":
+                print('Stop streaming')
+                gateway.shutdown()
+                break
+            elif command == "sync":
+                python_callback = PythonCallbackImpl(simple_fun)
+                nothread_executor = gateway.jvm.App(python_callback)
+                nothread_executor.runSynchronous()
+            elif command == "async":
+                python_callback = PythonCallbackImpl(simple_fun)
+                nothread_executor = gateway.jvm.App(python_callback)
+                nothread_executor.runAsynchronous()
+            else:
+                print(exec(command))
+        except KeyboardInterrupt:
+            print('Stop streaming')
+            gateway.shutdown()
+            break
+        except (SyntaxError, NameError) as e:
+            print('Stop streaming')
+            gateway.shutdown()
+            print(e)
+
+if __name__ == "__main__":
+    main()
+    sys.exit()
